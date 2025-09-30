96bab86a3f0f30e0528f553cf86443ae17f293b9
Zhe Feng <zhe.feng@pnnl.gov>
2025-09-29 17:26:27 -0700

    WIP: added some options in make_animation.sh.

--- make_animation.sh
+++ make_animation.sh
@@ -15,10 +15,14 @@ fig_height=8
 output_dpi=200
 
 # Figure directory
-figdir="/global/cfs/cdirs/m1867/zfeng/hk25/quicklooks/"${source_name}"/"
+# fig_basename="allmasks"
+fig_basename="cofmasks"
+# figdir="/global/cfs/cdirs/m1867/zfeng/hk25/quicklooks/"${source_name}"/"
+figdir="/global/cfs/cdirs/m1867/zfeng/hk25/quicklooks_cof/"${source_name}"/"
 # Animation parameters
 animation_dir="/global/cfs/cdirs/m1867/zfeng/hk25/animations/"
-animation_filename="${animation_dir}${source_name}_allmasks_$(echo ${start_date} | cut -d'T' -f1).mp4"
+# animation_filename="${animation_dir}${source_name}_allmasks_$(echo ${start_date} | cut -d'T' -f1).mp4"
+animation_filename="${animation_dir}${source_name}_${fig_basename}_$(echo ${start_date} | cut -d'T' -f1).mp4"
 
 # Activate Python environment
 source activate /global/common/software/m1867/python/hackathon
@@ -26,9 +30,12 @@ source activate /global/common/software/m1867/python/hackathon
 mkdir -p ${animation_dir}
 mkdir -p ${figdir}
 
-# Run plotting scrip
+# plotting_code="plot_feature_masks.py"
+plotting_code="plot_cooccurrence_masks.py"
+
+# Run plotting script
 echo "Running parallel processing with ${n_workers} workers"
-python plot_feature_masks.py \
+python ${plotting_code} \
     --source ${source_name} \
     --start ${start_date} \
     --end ${end_date} \
@@ -40,7 +47,7 @@ python plot_feature_masks.py \
 
 # Make animation using ffmpeg
 echo "Making animation from quicklook plots..."
-ffmpeg -framerate 2 -pattern_type glob -i "${figdir}${source_name}_allmasks_*.png" \
+ffmpeg -framerate 2 -pattern_type glob -i "${figdir}${source_name}_${fig_basename}_*.png" \
     -vf "scale=trunc(iw/2)*2:trunc(ih/2)*2" \
     -c:v libx264 -r 10 -crf 20 -pix_fmt yuv420p \
     -y ${animation_filename}
